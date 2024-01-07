18c19fea63a264b0c17268d1b41ca2f18c0d4dcd
Celina <60200600+cia161@users.noreply.github.com>
2024-01-06 18:51:26 -0600

    started the quiz and questions screen widgets - also rendered the activeScreen conditionally

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,2 +1,23 @@
 // return the material App
 // should be stateful widget
+
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+
+  const QuestionsScreen({super.key});
+  @override
+  State<QuestionsScreen> createState() {
+    
+    return _QuestionsScreenState();
+    
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  @override
+  Widget build(context) {
+    return const Text('QuestionsScreen');
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
+
+ class Quiz extends StatefulWidget {
+
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+ }
+
+class _QuizState extends State<Quiz> {
+
+  Widget activeScreen = const StartScreen(); 
+  //method that switches the screen
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+  
+  @override
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 97, 24, 166),
+                Color.fromARGB(255, 125, 60, 186),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
