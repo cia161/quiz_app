6c3b94062ba0146d7292233d40ba389feac55b42
Celina <60200600+cia161@users.noreply.github.com>
2024-01-09 09:11:38 -0600

    added and styled questions (just one)

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,12 @@
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers); // copy
+    shuffledList.shuffle(); // shuffled copy
+    return shuffledList;
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,22 +2,45 @@
 // should be stateful widget
 
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-
   const QuestionsScreen({super.key});
   @override
   State<QuestionsScreen> createState() {
-    
     return _QuestionsScreenState();
-    
   }
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity, // tells Dart to use as much width as possible
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // centers the column
+          crossAxisAlignment: CrossAxisAlignment.stretch, // stretch items as wide as possible
+          children: [
+             Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            // ... spreading values - spreads the list as individual comma separated valeus
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
